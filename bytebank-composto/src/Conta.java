02f47001548db1209ee4b721a4c001863c7811e6
Henrique Erinaldo <112641928+henriqueerds@users.noreply.github.com>
2023-07-06 11:27:02 -0300

    Uso dos Getters e Setters
    Os getters e setters são importantes no Java para garantir o encapsulamento de dados, permitindo acesso controlado às variáveis de uma classe, promovendo o princípio de encapsulamento e facilitando a manutenção e modificação futuras do código.

--- bytebank-composto/src/Conta.java
+++ bytebank-composto/src/Conta.java
@@ -2,7 +2,7 @@ public class Conta {
     private double saldo;
     private int agencia;
     private int numero;
-    Cliente titular;
+    private Cliente titular;
 
     public void deposita(double valor) {
         this.saldo += valor;
@@ -39,4 +39,13 @@ public class Conta {
     public void setAgencia (int agencia) {
         this.agencia = agencia;
     }
+
+    public Cliente getTitular() {
+        return titular;
+    }
+
+    public void setTitular (Cliente titular) {
+        this.titular = titular;
+    }
+
 }
--- bytebank-composto/src/Main.java
+++ bytebank-composto/src/Main.java
@@ -2,19 +2,21 @@ public class Main {
     public static void main(String[] args) {
 
         Cliente paulo = new Cliente();
-        paulo.nome = "Paulo Silveira";
-        paulo.cpf = "222-222-222-22";
-        paulo.profissao = "Programador";
+        paulo.setNome("Paulo Silveira");
+        paulo.setCpf("065.566.564-34");
+        paulo.setProfissao("Analista");
 
         Conta contaDoPaulo = new Conta();
         contaDoPaulo.deposita(1000);
         contaDoPaulo.setNumero(123432);
+        contaDoPaulo.setTitular(paulo);
+        contaDoPaulo.setAgencia(1234);
 
-        contaDoPaulo.titular = paulo;
-
-        System.out.println("Titular da Conta: "+ contaDoPaulo.titular.nome);
+        System.out.println("Titular da Conta: "+ contaDoPaulo.getTitular().getNome());
+        System.out.println("CPF: "+ contaDoPaulo.getTitular().getCpf());
+        System.out.println("Profissão: "+ contaDoPaulo.getTitular().getProfissao());
+        System.out.println("Agencia: "+ contaDoPaulo.getAgencia());
         System.out.println("Número da Conta: "+ contaDoPaulo.getNumero());
         System.out.println("Saldo da Conta: "+contaDoPaulo.getSaldo());
-
     }
 }
