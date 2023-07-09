86cafff7fccad7703cb38f7f902b57deaa916d1b
Henrique Erinaldo <112641928+henriqueerds@users.noreply.github.com>
2023-07-09 19:40:32 -0300

    Utilização do Abstract
    Foi usando o Abstract na classe e no método mãe, para assim, cada método tenha seu detalhe especifico e que não possa mais criar objetos mãe por ser muito generalista.

---
+++ AulaAlura/src/TI.java
@@ -0,0 +1,6 @@
+public class TI extends Funcionario{
+    public double getBonificacao () {
+
+        return super.getSalario() * 0.50;
+    }
+}
--- AulaAlura/src/TesteFuncionario.java
+++ AulaAlura/src/TesteFuncionario.java
@@ -1,6 +1,6 @@
 public class TesteFuncionario {
     public static void main(String[] args) {
-        Funcionario henrique = new Funcionario();
+        TI henrique = new TI();
         henrique.setNome("Henrique Erinaldo");
         henrique.setCpf("063.3434.3434-23");
         henrique.setSalario(2600.00);
